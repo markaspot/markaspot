67c5a50b475b26a6415c9cdef9d3dc0bb99a2f61
Holger Kreis <holger@markaspot.org>
2018-01-10 03:53:21 +0100

    Move scroll-to-top button.

--- modules/markaspot_blocks/src/Plugin/Block/MarkaspotSideBlock.php
+++ modules/markaspot_blocks/src/Plugin/Block/MarkaspotSideBlock.php
@@ -23,6 +23,8 @@ class MarkaspotSideBlock extends BlockBase {
       '#markup' => '
         <div class="mas-action">
           <ul class="theme-grey orientation-vertical col-3">
+            <li class="mas-button"><a class="scroll-to-top" href="#top" data-rel="popup" title=""
+             role="button" aria-label=""><span class="fa fa-arrow-circle-up"></span><span class="add">Scroll to top</span></a></li>
             <li class="mas-button"><a href="/" data-rel="popup" title="" role="button" aria-label=""><span class="fa fa-home"></span><span class="add">Add service request</span></a></li>
             <li class="mas-button"><a href="/report" data-rel="popup" title="" role="button" aria-label=""><span class="fa fa-pencil"></span><span class="add">Add service request</span></a></li>
             <li class="mas-button"><a href="/requests" data-rel="popup" title="" role="button" aria-label=""><span class="fa fa-eye"></span><span class="add">View service requestd</span></a></li>
