bb7ad167bc63e4381b5f40aea6c548f0ab8171f4
Holger Kreis <holger@markaspot.org>
2018-11-25 16:18:19 +0100

    Clean up

--- themes/custom/masradix/assets/js/masradix.script.js
+++ themes/custom/masradix/assets/js/masradix.script.js
@@ -17,7 +17,6 @@
     } else {
       $('.fixed-header').show();
       $('img.site-logo').fadeIn(800);
-
     }
 
   }
@@ -30,7 +29,6 @@
     Pace.restart();
   });
   $(document).ready(function () {
-
     toDesktop($(window).width());
     $(window).resize(function() {
       toDesktop($(this).width());
