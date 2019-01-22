0fcc88ca868f82865891c934cf661ec649d6c9cb
Holger Kreis <holger@markaspot.org>
2019-01-22 17:06:01 +0100

    Fix access check for nodes when running archiving cron

--- modules/markaspot_archive/src/ArchiveService.php
+++ modules/markaspot_archive/src/ArchiveService.php
@@ -33,23 +33,57 @@ class ArchiveService implements ArchiveServiceInterface {
     $this->configFactory = $config_factory;
   }
 
+  function array_flatten($array) {
+    $result = [];
+    foreach ($array as $key => $value) {
+      array_push($result,$value);
+    }
+    return $result;
+  }
+
   /**
    * @return \Drupal\Core\Entity\EntityInterface[]
    */
   public function load() {
     $config = $this->configFactory->get('markaspot_archive.settings');
     $days = strtotime(' - ' . $config->get('days') . 'days');
-
+    $tids = $this->array_flatten($config->get('status_archivable'));
     $storage = $this->entityTypeManager->getStorage('node');
 
     $query = $storage->getQuery()
-      ->condition('changed', $days, '<')
-      ->condition('type', 'service_request');
-    $and = $query->andConditionGroup();
-    $and->condition('field_status.entity.tid', $config->get('status_archivable'));
+      ->condition('changed', $days, '<=')
+      ->condition('type', 'service_request')
+      ->condition('field_status', $tids, 'IN');
+    $query->accessCheck(FALSE);
+
     $nids = $query->execute();
     return $storage->loadMultiple($nids);
   }
 
 
 }
+
+
+/*
+ *
+ *
+
+
+public function load() {
+  $config = $this->configFactory->get('markaspot_archive.settings');
+  $timestamp_from = strtotime(' - ' . $config->get('days') . 'days');
+  $timestamp_from = strtotime(' - ' . 1  . 'days');
+
+  $storage = $this->entityTypeManager->getStorage('node');
+
+  $query = $storage->getQuery()
+    //->condition('changed', $timestamp_from, '<')
+    //->condition('type', 'service_request')
+    ->condition('field_status', 15, 'IN');
+
+  $nids = $query->execute();
+
+
+
+  *
+ * */
