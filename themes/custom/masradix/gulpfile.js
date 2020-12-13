6c537a144792ad686bb46c29212043aaf8c82cdb
Holger Kreis <holger@markaspot.org>
2020-12-13 17:59:53 +0100

    Update theme to use Gulp 4.x in development

--- themes/custom/masradix/gulpfile.js
+++ themes/custom/masradix/gulpfile.js
@@ -17,9 +17,21 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 
+
+// Compress images.
+gulp.task('images', () =>
+  gulp.src(config.images.src)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngcrush()]
+    }))
+    .pipe(gulp.dest(config.images.dest))
+);
+
 // CSS.
-gulp.task('css', function() {
-  return gulp.src(config.css.src)
+gulp.task('css', gulp.series('images', () =>
+  gulp.src(config.css.src)
     .pipe(glob())
     .pipe(plumber({
       errorHandler: function (error) {
@@ -40,43 +52,48 @@ gulp.task('css', function() {
     .pipe(autoprefix('last 2 versions', '> 1%', 'ie 9', 'ie 10'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(config.css.dest))
-    .pipe(browserSync.reload({ stream: true, match: '**/*.css' }));
-});
+    .pipe(browserSync.reload({ stream: true, match: '**/*.css' }))
+));
 
-// Compress images.
-gulp.task('images', function () {
-  return gulp.src(config.images.src)
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      use: [pngcrush()]
-    }))
-    .pipe(gulp.dest(config.images.dest));
-});
+
 
 // Fonts.
-gulp.task('fonts', function() {
-  return gulp.src(config.fonts.src)
-    .pipe(gulp.dest(config.fonts.dest));
-});
+gulp.task('fonts', () =>
+  gulp.src(config.fonts.src)
+    .pipe(gulp.dest(config.fonts.dest))
+);
 
 // Watch task.
-gulp.task('watch', function() {
-  gulp.watch(config.css.src, ['css']);
-  gulp.watch(config.images.src, ['images']);
-});
+gulp.task('watch', () =>
+  gulp.watch(config.css.src, gulp.series('css'))
+);
 
 // Static Server + Watch
-gulp.task('serve', ['css', 'fonts', 'watch'], function() {
+
+gulp.task('serve', gulp.series('css', 'fonts', 'watch', () =>
   browserSync.init({
     proxy: config.proxy
-  });
-});
+  })
+));
 
 // Run drush to clear the theme registry.
-gulp.task('drush', shell.task([
+gulp.task('drush', () =>
+   shell.task([
   'drush cache-clear theme-registry'
-]));
+  ]
+ ));
 
 // Default Task
-gulp.task('default', ['serve']);
+gulp.task('default', gulp.series('css', 'serve', (done) => {
+
+  // image changes
+  gulp.watch(config.images.src, gulp.series('images'));
+
+  // CSS changes
+  gulp.watch(config.css.src.watch, gulp.series('css'));
+
+  done();
+
+}));
+
+// gulp.task('default', gulp.series('default'));
