1100ff2726e09d223fd125b1c06963e581ef04d8
Holger Kreis <holger@markaspot.org>
2021-10-07 10:59:20 +0200

    Fix caching issue with error responses

--- modules/markaspot_open311/src/EventSubscriber/GeoreportException.php
+++ modules/markaspot_open311/src/EventSubscriber/GeoreportException.php
@@ -3,6 +3,7 @@
 namespace Drupal\markaspot_open311\EventSubscriber;
 
 use Drupal\rest\ResourceResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -63,20 +64,26 @@ class GeoreportException implements EventSubscriberInterface {
         $errors['error']['code'] = $exceptionCode;
         $errors['error']['description'] = $exception->getMessage();
       }
-      /*
+
       $request_format = pathinfo($current_path, PATHINFO_EXTENSION);
       $request_format = isset($request_format) ? $request_format : 'html';
 
       if ($request_format == 'json' || $request_format == 'xml') {
         $content = \Drupal::service('serializer')
           ->serialize($errors, $request_format);
+
+
       }
-      */
+
+
+
 
 
       // Create response, set status code etc.
       $status_code = ($exceptionCode == 0) ? 500 : $exceptionCode;
-      $response = new ResourceResponse($errors, $status_code);
+      // $response = new ResourceResponse($errors, $status_code);
+      $response = new Response($content, $status_code);
+      $response->headers->set('Content-Type', 'application/' . $request_format);
 
       // $response->setContent($content);
 
