896540bdc08e0069e5262d15c50d693fb8a5a638
Holger Kreis <holger@markaspot.org>
2021-11-22 07:54:41 +0100

    Fix boilerplate js

--- modules/markaspot_boilerplate/js/markaspot_boilerplate.js
+++ modules/markaspot_boilerplate/js/markaspot_boilerplate.js
@@ -4,7 +4,8 @@
     attach: function (context, settings) {
       $('select[name*=boilerplate]').change(function () {
         const url = '/markaspot_boilerplate/load/' + this.value
-        const $textarea = $(this).closest('.paragraphs-subform').find('textarea')
+        let $textarea = $(this).closest('.paragraphs-subform').find('textarea')
+        $textarea = $textarea.length > 0 ? $textarea : $(this).closest('#edit-group-service-provider').find('textarea')
         $.getJSON(url, function (data) {
           CKEDITOR.instances[$textarea.attr('id')].setData(data)
         })
