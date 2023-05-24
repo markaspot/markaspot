8e4347d0952a8f3b5ac02afdb555aa3ac62427e1
Holger Kreis <holger@markaspot.org>
2023-05-24 05:41:28 +0200

    Fix import for default_content

--- modules/markaspot_boilerplate/js/markaspot_boilerplate.js
+++ modules/markaspot_boilerplate/js/markaspot_boilerplate.js
@@ -2,20 +2,34 @@
 
   Drupal.behaviors.markaspot_boilerplate = {
     attach: function (context, settings) {
-      var $boilerplate = $(context).find('select[name*=boilerplate]').once('markaspot_boilerplate');
-      if ($boilerplate.length) {
-        $('select[name*=boilerplate]').change(function () {
-          const url = '/markaspot_boilerplate/load/' + this.value
-          let $textarea = $(this).closest('.paragraphs-subform').find('textarea')
-          $textarea = $textarea.length > 0 ? $textarea : $(this).closest('#edit-group-service-provider').find('textarea')
-          $.getJSON(url, function (data) {
-            CKEDITOR.instances[$textarea.attr('id')].setData(data)
-          })
-        })
-      }
+      const boilerplateElements = once('markaspot_boilerplate', context.querySelectorAll('select[name*=boilerplate]'));
+      boilerplateElements.forEach(el => {
+        el.addEventListener('change', function() {
+          const url = '/markaspot_boilerplate/load/' + this.value;
+          let $textarea = $(this).closest('.paragraphs-subform').find('textarea');
+          console.log(Drupal.CKEditor5Instances);
+          if ($textarea.length > 0) {
+            let instanceId = $textarea.data('ckeditor5-id');  // the id you are looking for
+            if (Drupal.CKEditor5Instances.has(instanceId)) {
+              const editor = Drupal.CKEditor5Instances.get(instanceId);
+              // Use editor instance here...
+            } else {
+              console.log('CKEditor instance not found for', instanceId);
+            }
+            //const editor = Drupal.CKEditor5Instances.get($textarea.data('ckeditor5-id'));
+            if (editor) {
+              $.getJSON(url, function (data) {
+                editor.setData(data);
+              });
+            } else {
+              console.log('CKEditor instance not found for', $textarea.attr('id'));
+            }
+          } else {
+            console.log('Textarea not found');
+          }
+        });
+      });
     }
   }
 
-}(jQuery, Drupal, drupalSettings))
-
-
+}(jQuery, Drupal, drupalSettings));
