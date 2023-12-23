38e960cbfbaaab82c87d89004a43eb3b80a87d76
Holger Kreis <holger@markaspot.org>
2023-12-23 06:30:34 +0100

    Switch to event subscriber to check for json and xml request format

---
+++ modules/markaspot_open311/src/EventSubscriber/RequestFormatSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\markaspot_open311\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpFoundation\Response;
+
+class RequestFormatSubscriber implements EventSubscriberInterface {
+
+  public static function getSubscribedEvents() {
+    // The priority must be high enough to run before the routing listener.
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest', 100],
+    ];
+  }
+
+  public function onKernelRequest(RequestEvent $event) {
+    $request = $event->getRequest();
+    $pathInfo = $request->getPathInfo();
+
+    // Check if the path corresponds to the georeport v2 standard.
+    if (preg_match('/\/georeport\/v2\/.*\.(json|xml)$/', $pathInfo, $matches)) {
+      // Set the request format based on the suffix.
+      $request->setRequestFormat($matches[1]);
+    }
+  }
+}
